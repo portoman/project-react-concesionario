720a0631b137bb2cf72f60e8b0b4a4b79eb523d3
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-25 09:00:49 +0100

    Include const listCar

--- src/components/CarCarrousel/CarCarrousel.jsx
+++ src/components/CarCarrousel/CarCarrousel.jsx
@@ -1,27 +1,28 @@
 import { useEffect, useState, useRef } from 'react';
 
 import CarComponent from '../CarComponent/CarComponent';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function CarCarrousel({ vehiculos }) {
 
-
-    const componentCar = vehiculos.map(
-        element => <div className="">
-            <h2>{element.km}</h2>
-            <h2>{element.modelo}</h2>
-        </div>
+/*
+Constante que almacena un nuevo array con la lista de vehiculos a partir de un map
+*/
+    const listCar = vehiculos.map(
+        element => 
+            <CarComponent vehiculo={element} />
+        
     );
 
     return (
-        <p>
+        <>
             <div id="carouselVentas" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <div id="botonesVenta"></div>
                 </div>
-                <div class="carousel-inner">
-                    <CarComponent vehiculo={vehiculos[0]} />
-                    <CarComponent vehiculo={vehiculos[1]} />
-                    <CarComponent vehiculo={vehiculos[2]} />
+                <div class="carousel-inner">                    
+                    {listCar}
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselVentas"
                     data-bs-slide="prev">
@@ -34,7 +35,45 @@ function CarCarrousel({ vehiculos }) {
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
-        </p>
+
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=First slide&bg=373940"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
 
     );
 }
