24b193ca92b6847268662b6c6e8114061ee98c14
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-31 09:38:41 +0200

    Add more vehicules

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
-  const ruta="http://127.0.0.1:5500/project-react/src/img/";
+  const ruta = "http://127.0.0.1:5500/project-react/src/img/";
 
   const [vehiculosArray, setVehiculosArray] = useState([
     {
@@ -15,38 +15,67 @@ function App() {
       modelo: "Corsa",
       precio: "12.900 €",
       km: "5000 km",
-      img: ruta+"opelAstra.jpg",
-      portada: true,
+      img: ruta + "opelAstra.jpg",
       venta: true
-  },
-  {
+    },
+    {
       marca: "Ford",
       modelo: "Mondeo",
       precio: "15.900 €",
       km: "8500 km",
-      img: ruta+"fordMondeo.jpg",
-      portada: false,
+      img: ruta + "fordMondeo.jpg",
       venta: true
-  },
-  {
-    marca: "Peugeot",
-    modelo: "Partner",
-    precio: "8.900 €",
-    km: "2500 km",
-    img: ruta+"peugeotPartner.jpg",
-    portada: false,
-    venta: true
-}
+    },
+    {
+      marca: "Peugeot",
+      modelo: "Partner",
+      precio: "8.900 €",
+      km: "2500 km",
+      img: ruta + "peugeotPartner.jpg",
+      venta: true
+    },
+    {
+      marca: "Nissan",
+      modelo: "Qashqai",
+      precio: "10.900 €",
+      km: "5500 km",
+      img: ruta + "nissanQashqai.jpg",
+      venta: true
+    },
+    {
+      marca: "Peugeot",
+      modelo: "2008",
+      precio: "12.900 €",
+      km: "1500 km",
+      img: ruta + "peugeot2008.jpg",
+      venta: false
+    },
+    {
+      marca: "Seat",
+      modelo: "Ibiza",
+      precio: "5.900 €",
+      km: "2500 km",
+      img: ruta + "seatIbiza.jpg",
+      venta: false
+    },
+    {
+      marca: "VolksWagen",
+      modelo: "Polo",
+      precio: "7.900 €",
+      km: "10500 km",
+      img: ruta + "vwPolo.jpg",
+      venta: false
+    }
   ]);
 
   return (
     <>
-    <h1>Concesionario Vioño</h1>
-    <NavBar  vehiculos={vehiculosArray}/>
-    <h3>Ofertas alquiler</h3>
-    <CarCarrousel   vehiculos={vehiculosArray}/>
+      <h1>Concesionario Vioño</h1>
+      <NavBar vehiculos={vehiculosArray} />
+      <h3>Ofertas alquiler</h3>
+      <CarCarrousel vehiculos={vehiculosArray} />
     </>
-   
+
   );
 }
 
