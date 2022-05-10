3cea8b83f7103770804ecd46ab668df38b0e9b71
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-10 12:41:19 +0200

    Remove trials array

--- 02_frontend/src/App.js
+++ 02_frontend/src/App.js
@@ -27,90 +27,13 @@ function App() {
       data => actions.setState(data)
     )
   }
-
-  const vehiculosArray = [
-    {
-      marca: "Opel",
-      modelo: "Corsa",
-      precio: "12.900 €",
-      km: "5000 km",
-      img: ruta + "opelAstra.jpg",
-      venta: true
-    },
-    {
-      marca: "Ford",
-      modelo: "Mondeo",
-      precio: "15.900 €",
-      km: "8500 km",
-      img: ruta + "fordMondeo.jpg",
-      venta: true
-    },
-    {
-      marca: "Peugeot",
-      modelo: "408",
-      precio: "8.900 €",
-      km: "2500 km",
-      img: ruta + "peugeotPartner.jpg",
-      venta: true
-    },
-    {
-      marca: "Peugeot",
-      modelo: "Partner",
-      precio: "8.900 €",
-      km: "2500 km",
-      img: ruta + "peugeotPartner.jpg",
-      venta: false
-    }
-    ,
-    {
-      marca: "Nissan",
-      modelo: "Qashqai",
-      precio: "10.900 €",
-      km: "5500 km",
-      img: ruta + "nissanQashqai.jpg",
-      venta: true
-    },
-    {
-      marca: "Peugeot",
-      modelo: "2008",
-      precio: "12.900 €",
-      km: "1500 km",
-      img: ruta + "peugeot2008.jpg",
-      venta: false
-    },
-    {
-      marca: "Seat",
-      modelo: "Ibiza",
-      precio: "5.900 €",
-      km: "2500 km",
-      img: ruta + "seatIbiza.jpg",
-      venta: false
-    },
-    {
-      marca: "Seat",
-      modelo: "Arona",
-      precio: "5.900 €",
-      km: "2500 km",
-      img: ruta + "seatIbiza.jpg",
-      venta: false
-    },
-    {
-      marca: "VolksWagen",
-      modelo: "Polo",
-      precio: "7.900 €",
-      km: "10500 km",
-      img: ruta + "vwPolo.jpg",
-      venta: false
-    }
-  ];
-
-
+  
   return (
     <>
       <h1>Concesionario Vioño</h1>
       <NavBar />
       <Routes>
-        <Route path="/" element={<CarCarrousels vehiculos={vehiculosArray} />} />
+        <Route path="/" element={<CarCarrousels />} />
         /*Añadir un nuevo parametro conforme si es venta o no*/
         <Route path="/vehiculo/:id" element={<IndividualPage />} />
       </Routes>
