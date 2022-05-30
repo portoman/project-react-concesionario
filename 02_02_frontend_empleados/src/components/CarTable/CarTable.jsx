13b9659595925afe14cd51f97c6ed73af0f80775
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-30 13:30:34 +0200

    Get all cars

--- 02_02_frontend_empleados/src/components/CarTable/CarTable.jsx
+++ 02_02_frontend_empleados/src/components/CarTable/CarTable.jsx
@@ -1,7 +1,28 @@
-
+import { useEffect, useState, useRef, useContext } from 'react';
 import Table from 'react-bootstrap/Table'
+import { Context } from "../../SharedState"
 
 function CarTable() {
+
+    const { state } = useContext(Context);
+
+
+    const vehiculos = state.map(
+        (element, index) =>
+
+            <tr>
+                <td>{index}</td>
+                <td>{element.matricula}</td>
+                <td>{element.modelo}</td>
+                <td>{element.marca}</td>
+                <td>{element.km}</td>
+                <td>{element.precio}</td>
+                <td>{element.cilindrada}</td>
+                <td>{element.disponible}</td>
+                <td>{element.alquiler}</td>
+                <td>{element.oferta}</td>
+            </tr>
+    );
 
     return (
 
@@ -12,42 +33,19 @@ function CarTable() {
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>Table heading</th>
-                            <th>Table heading</th>
-                            <th>Table heading</th>
-                            <th>Table heading</th>
-                            <th>Table heading</th>
-                            <th>Table heading</th>
+                            <th>Matrícula</th>
+                            <th>Modelo</th>
+                            <th>Marca</th>
+                            <th>Km</th>
+                            <th>Precio</th>
+                            <th>Cilindrada</th>
+                            <th>Disponible</th>
+                            <th>Alquiler</th>
+                            <th>Oferta</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        </tr>
+                        {vehiculos}                          
                     </tbody>
                 </Table>
             </div>
