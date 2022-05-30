ab5f0de5e91a68cc9d3721806e127c6b1fbe8c73
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-30 12:18:15 +0200

    Create context

--- 02_02_frontend_empleados/src/App.js
+++ 02_02_frontend_empleados/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useContext, useEffect } from "react"
+import { Context } from './SharedState';
+import { get } from "./aux_api";
+import { Link } from 'react-router-dom';
+
 
 function App() {
+
+  const { state, actions } = useContext(Context);
+  
+  useEffect(
+    () => {
+      getCars()
+    },
+
+    []
+  )
+
+  /*Llamada al fetch de get donde se incluyen los datos de todos los coches de la base
+  de datos en el setState*/
+  function getCars() {
+    get("http://localhost:3000/api" + "/allCoches/").then(
+      data => actions.setState(data)
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Concesionario Vioño</h1>
+    </>
   );
 }
 
---
+++ 02_02_frontend_empleados/src/aux_api.js
@@ -0,0 +1,26 @@
+
+export const HOST = "/api"
+
+//Funcion para coger coches GET
+export async function get(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+
+
+export async function post(url, data) {
+    const response = await fetch(
+        url,
+        {
+            method: 'POST',
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    );
+    const responseData = await response.json();
+    return responseData;
+}
