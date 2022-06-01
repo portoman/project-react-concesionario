41eb050fb754f1bb0439e2a3616747df14369c20
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-01 11:37:32 +0200

    Create Form in MainForm

--- 02_02_frontend_empleados/src/components/ModificateCar/ModificateCar.jsx
+++ 02_02_frontend_empleados/src/components/ModificateCar/ModificateCar.jsx
@@ -1,18 +1,91 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
+import { useEffect, useState, useRef, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { Context } from "../../SharedState"
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 
 function ModificateCar() {
 
+    const { states, actions } = useContext(Context);
+
+    //Variable para coger datos de la URL
+    const parms = useParams();
+    let numero = parseInt(parms.id);
+
+    //Buscamos el coche en el state que coincida con el parametro de la url, para poder mostrarlo
+    const coche = states.cars.find(element => element.id_coche === numero);
+    function unoSiCeroNo(numero) {
+        let texto = "";
+        if (numero == 1) {
+            texto = "Sí"
+        } else {
+            texto = "No"
+        }
+        return texto;
+    }
+
+    function alquilerVenta(numero) {
+        let texto = "";
+        if (numero == 1) {
+            texto = "Alquiler"
+        } else {
+            texto = "Venta"
+        }
+        return texto;
+    }
+
     return (
 
         <>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Matricula</Form.Label>
+                    <Form.Control type="email" placeholder={coche.matricula} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Modelo</Form.Label>
+                    <Form.Control type="email" placeholder={coche.modelo} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Marca</Form.Label>
+                    <Form.Control type="email" placeholder={coche.marca} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Km</Form.Label>
+                    <Form.Control type="email" placeholder={coche.km} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Precio</Form.Label>
+                    <Form.Control type="email" placeholder={coche.precio} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Cilindrada</Form.Label>
+                    <Form.Control type="email" placeholder={coche.cilindrada} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Disponible</Form.Label>
+                    <Form.Control type="email" placeholder={unoSiCeroNo(coche.disponible)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Alquiler</Form.Label>
+                    <Form.Control type="email" placeholder={alquilerVenta(coche.alquiler)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Oferta</Form.Label>
+                    <Form.Control type="email" placeholder={unoSiCeroNo(coche.oferta)} />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
 
         </>
     );
 }
+
 
 
 
