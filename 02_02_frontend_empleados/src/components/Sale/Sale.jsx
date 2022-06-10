93585233fba0ca6f3c473936c6c32b9d2d486685
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-10 13:41:40 +0200

    Set Layout of Sale

--- 02_02_frontend_empleados/src/components/Sale/Sale.jsx
+++ 02_02_frontend_empleados/src/components/Sale/Sale.jsx
@@ -14,9 +14,12 @@ function Sale() {
 
     const { states, actions } = useContext(Context);
 
+    //Mediante el método filter, segrego los coches que están disponibles 
+    const cochesDisponibles = states.cars.filter(element => element.disponible === 1);
 
+    
     const [fecha, setFecha] = useState("");
-    const [id_coche, setIdcoche] = useState(states.cars[0].id_coche);
+    const [id_coche, setIdcoche] = useState(0);
     const [id_cliente, setIdCliente] = useState("");
     const [precio, setPrecio] = useState("");
 
@@ -45,56 +48,44 @@ function Sale() {
 
         <>
             <h2 className="mx-auto">Venta</h2>
-            <Container>
-                <Form className="col-8 mx-auto">
-                    <Row>
-                        <Form.Group className="col-3 mb-3" controlId="formBasicEmail">
-                            <Stack direction="horizontal" gap={2}>
-                                <Form.Label>Fecha</Form.Label>
-                                <Form.Control type="text" onChange={fechaInputChangeHandler} />
-                            </Stack>
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Stack direction="horizontal" gap={2}>
-                                <Form.Label>Coche</Form.Label>
-                                <Form.Select aria-label="Default select example" onChange={idCocheInputChangeHandler}>
-                                    {states.cars.map(
-                                        (element, index) =>
-                                            <option value={element.id_coche}>Id: {element.id_coche} - Matricula: {element.matricula} - Modelo: {element.modelo} - Marca: {element.marca}</option>
-                                    )}
-                                </Form.Select>
-                            </Stack>
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Stack direction="horizontal" gap={2}>
-                                    <Form.Label>Cliente</Form.Label>
-                                    <Form.Control type="number" onChange={idClienteChangeHandler} />
-                                </Stack>
-                            </Form.Group>
-                        </Col>
-                        <Col>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Stack direction="horizontal" gap={2}>
-                                    <Form.Label>Precio</Form.Label>
-                                    <Form.Control type="number" onChange={precioInputChangeHandler} />
-                                </Stack>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Stack>
-                        <div className="ms-auto">
-                            <Link to="/carTable"><Button onClick={clickHandler} variant="primary" type="submit">
-                                Registrar
-                            </Button></Link>
-                        </div>
+            <Form className="col-8 mx-auto">
+                <Form.Group className="col-3 mb-3" controlId="formBasicEmail">
+                    <Stack direction="horizontal" gap={2}>
+                        <Form.Label>Fecha</Form.Label>
+                        <Form.Control type="date" onChange={fechaInputChangeHandler} />
                     </Stack>
-                </Form>
-            </Container>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Stack direction="horizontal" gap={2}>
+                        <Form.Label>Coche</Form.Label>
+                        <Form.Select aria-label="Default select example" onChange={idCocheInputChangeHandler}>
+                            {cochesDisponibles.map(
+                                (element, index) =>
+                                    <option value={element.id_coche}>Id: {element.id_coche} - Matricula: {element.matricula} - Modelo: {element.modelo} - Marca: {element.marca}</option>
+                            )}
+                        </Form.Select>
+                    </Stack>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Stack direction="horizontal" gap={2}>
+                        <Form.Label>Cliente</Form.Label>
+                        <Form.Control type="number" onChange={idClienteChangeHandler} />
+                    </Stack>
+                </Form.Group>
+                <Form.Group className="col-3 mb-3" controlId="formBasicEmail">
+                    <Stack direction="horizontal" gap={2}>
+                        <Form.Label>Precio</Form.Label>
+                        <Form.Control type="number" onChange={precioInputChangeHandler} />
+                    </Stack>
+                </Form.Group>
+                <Stack>
+                    <div className="ms-auto">
+                        <Link to="/carTable"><Button onClick={clickHandler} variant="primary" type="submit">
+                            Registrar
+                        </Button></Link>
+                    </div>
+                </Stack>
+            </Form>
         </>
     );
 }
