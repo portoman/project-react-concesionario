c4a5249e1ec44c6e092f52cbeec2776dc78f0d6d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-15 12:57:57 +0200

    Export functions to local/functions.js

--- 02_02_frontend_empleados/src/components/SaleTable/SaleTable.jsx
+++ 02_02_frontend_empleados/src/components/SaleTable/SaleTable.jsx
@@ -4,6 +4,7 @@ import { Context } from "../../SharedState"
 import Button from 'react-bootstrap/Button'
 import { apiDelete, post } from "../../aux_api";
 import { Link } from 'react-router-dom';
+import { takeMatricula, takeModelo, takeMarca, takeDNI, takeNombre, takeApellidos } from "../../local/functions";
 
 function SaleTable() {
 
@@ -18,64 +19,6 @@ function SaleTable() {
         actions.getAllCars();
     }
 
-    /**
-     * Función para obtener la matrícula a partir del id del coche
-     * @param {*} id 
-     * @returns matricula
-     */
-    function takeMatricula(id) {
-        let coche = states.cars.filter(element => element.id_coche == id);
-        return coche[0].matricula;
-    }
-    /**
-     * Función para obtener el modelo a partir del id del coche
-     * @param {*} id id del coche
-     * @returns modelo
-     */
-    function takeModelo(id) {
-        let coche = states.cars.filter(element => element.id_coche == id);
-        return coche[0].modelo;
-    }
-    /**
-     * Función para obtener la marca a partir del id del coche
-     * @param {*} id id del coche
-     * @returns marca
-     */
-    function takeMarca(id) {
-        let coche = states.cars.filter(element => element.id_coche == id);
-        return coche[0].marca;
-    }
-
-    /**
-     * Función para obtener el DNI a partir del id del cliente
-     * @param {*} id id del cliente
-     * @returns DNI
-     */
-    function takeDNI(id) {
-        let cliente = states.clients.filter(element => element.id_client == id);
-        return cliente[0].DNI;
-    }
-
-    /**
-     * Función para obtener el nombre a partir del id del cliente
-     * @param {*} id id del cliente
-     * @returns nombre
-     */
-     function takeNombre(id) {
-        let cliente = states.clients.filter(element => element.id_client == id);
-        return cliente[0].nombre;
-    }
-
-     /**
-     * Función para obtener los Apellidos a partir del id del cliente
-     * @param {*} id id del cliente
-     * @returns apellidos
-     */
-      function takeApellidos(id) {
-        let cliente = states.clients.filter(element => element.id_client == id);
-        return cliente[0].apellidos;
-    }
-    
 
     return (
 
@@ -106,13 +49,13 @@ function SaleTable() {
                                     <td>{element.id}</td>
                                     <td>{element.fecha}</td>
                                     <td>{element.id_coche}</td>
-                                    <td>{takeMatricula(element.id_coche)}</td>
-                                    <td>{takeModelo(element.id_coche)}</td>
-                                    <td>{takeMarca(element.id_coche)}</td>
+                                    <td>{takeMatricula(element.id_coche, states.cars)}</td>
+                                    <td>{takeModelo(element.id_coche, states.cars)}</td>
+                                    <td>{takeMarca(element.id_coche, states.cars)}</td>
                                     <td>{element.id_cliente}</td>
-                                    <td>{takeDNI(element.id_client)}</td>
-                                    <td>{takeNombre(element.id_client)}</td>
-                                    <td>{takeApellidos(element.id_client)}</td>
+                                    <td>{takeDNI(element.id_client, states.clients)}</td>
+                                    <td>{takeNombre(element.id_client, states.clients)}</td>
+                                    <td>{takeApellidos(element.id_client, states.clients)}</td>
                                     <td>{element.precio}</td>
                                     <td><Button onClick={() => { clickHandlerDelete(element.id) }} variant="primary" type="submit">
                                         Eliminar
---
+++ 02_02_frontend_empleados/src/local/functions.js
@@ -0,0 +1,62 @@
+
+/**
+    * Función para obtener la matrícula a partir del id del coche
+    * @param {*} id 
+    * @returns matricula
+    */
+export function takeMatricula(id, coches) {
+    let coche = coches.filter(element => element.id_coche == id);
+    return coche[0].matricula;
+}
+
+/**
+     * Función para obtener el modelo a partir del id del coche
+     * @param {*} id id del coche
+     * @returns modelo
+     */
+export function takeModelo(id, coches) {
+    let coche = coches.filter(element => element.id_coche == id);
+    return coche[0].modelo;
+}
+
+/**
+ * Función para obtener la marca a partir del id del coche
+ * @param {*} id id del coche
+ * @returns marca
+ */
+export function takeMarca(id, coches) {
+    let coche = coches.filter(element => element.id_coche == id);
+    return coche[0].marca;
+}
+
+ 
+
+    /**
+     * Función para obtener el DNI a partir del id del cliente
+     * @param {*} id id del cliente
+     * @returns DNI
+     */
+     export function takeDNI(id, clientes) {
+        let cliente = clientes.filter(element => element.id_client == id);
+        return cliente[0].DNI;
+    }
+
+    /**
+     * Función para obtener el nombre a partir del id del cliente
+     * @param {*} id id del cliente
+     * @returns nombre
+     */
+     export function takeNombre(id,clientes) {
+        let cliente = clientes.filter(element => element.id_client == id);
+        return cliente[0].nombre;
+    }
+
+     /**
+     * Función para obtener los Apellidos a partir del id del cliente
+     * @param {*} id id del cliente
+     * @returns apellidos
+     */
+     export function takeApellidos(id,clientes) {
+        let cliente = clientes.filter(element => element.id_client == id);
+        return cliente[0].apellidos;
+    }
