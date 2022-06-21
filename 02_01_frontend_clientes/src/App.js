072253fd131b3569cea6de9e9385472e4f73080d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-21 13:18:21 +0200

    Create LogIn component

--- 02_01_frontend_clientes/src/App.js
+++ 02_01_frontend_clientes/src/App.js
@@ -3,11 +3,15 @@ import NavBar from './components/NavBar/NavBar';
 import IndividualPage from './components/IndividualPage/IndividualPage';
 import CarCarrousels from './components/CarCarrousels/CarCarrousels';
 import MainForm from './components/MainForm/MainForm';
+import LogIn from './components/LogIn/LogIn'
 import { Route, Routes } from 'react-router-dom';
 import { useContext, useEffect } from "react"
 import { Context } from "./SharedState"
 import { get } from "./aux_api";
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+
 function App() {
 
   const ruta = "http://127.0.0.1:5500/02_01_frontend_clientes/src/img/";
@@ -28,15 +32,19 @@ function App() {
       data => actions.setState(data)
     )
   }
-  
+
   return (
     <>
-      <Link to={"/"}><h1>Concesionario Vioño</h1></Link>
+      <Stack direction="horizontal">
+        <h1 className="mx-auto"><Link to={"/"}>Concesionario Vioño</Link></h1>
+        <Link to={"/login"}><Button className="ms-auto">Usuarios</Button></Link>
+      </Stack>
       <NavBar />
       <Routes>
         <Route path="/" element={<CarCarrousels />} />
         <Route path="/vehiculo/:id" element={<IndividualPage />} />
         <Route path="/mainform" element={<MainForm />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
     </>
 
---
+++ 02_01_frontend_clientes/src/components/LogIn/LogIn.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState, useRef, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { Context } from "../../SharedState"
+import { PATH } from '../../defines';
+
+function Login() {
+
+
+    return (
+        <>
+            <h2>Marca</h2>
+        </>
+
+    );
+}
+
+export default Login;
