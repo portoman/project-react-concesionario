3ba4f536eec886305e2c75955e9045365b2cb18c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-22 09:43:17 +0200

    Include authorization in backend

--- 03_backend/src/index.mjs
+++ 03_backend/src/index.mjs
@@ -62,6 +62,64 @@ try {
   app.put(PATH_PREFIX + "/rent/", jsonParser, putRentController);
   app.delete(PATH_PREFIX + "/rent/", jsonParser, deleteRentController);
 
+  //Autorización
+  const secret = "abc123.secreto.serio" // Esto debería de estar en un .env
+
+  const user = {
+    username: "Alfonso",
+    password: "123",
+    accessLevel: 0,
+  }
+
+  function decodeBasicToken(request) {
+    const [authType, b64token] = request.headers.authorization.split(" ")
+    const tokenBuffer = new Buffer.from(b64token, "base64")
+    const token = tokenBuffer.toString()
+    return token.split(":")
+  }
+
+  function authMiddleware(req, res, next) {
+    try {
+      const [method, token] = req.headers.authorization.split(" ")
+      const { level } = jwt.verify(token, secret)
+      res.locals.level = level
+      next()
+    } catch (err) {
+      res.sendStatus(401)
+    }
+  }
+
+  //1. Endpoint Autenticación
+  app.get("/login/", (req, res) => {
+    const [username, password] = decodeBasicToken(req)
+    if (
+      /* Aquí iria la comprobación de que el usuario y pass existan
+     Si hay base de datos, y el usuario y contraseña están en la base de datos,
+     aquí meteríamos una query de verificación
+     */
+      username === user.username && password === user.password
+    ) {
+      //Creación de token/firma con un secret
+      const token = jwt.sign(
+        {
+          level: user.accessLevel
+        },
+        secret,
+        {
+          expiresIn: "1h",
+        }
+      )
+      res.send(token)
+    } else {
+      res.sendStatus(401)
+    }
+  })
+
+  //2. Uso del token
+  app.get("/secretos/", authMiddleware, (req, res) => {
+    res.send(`El secreto de la vida, el universo y de todo: 42`)
+  })
+
   app.listen(process.env.PORT || 3000, () => {
     console.log("Express running...");
   });
