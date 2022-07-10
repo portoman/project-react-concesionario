1ed5f51beeef886400d513f55c6f66268f21b982
portobuj <74266503+portoman@users.noreply.github.com>
2022-07-10 13:14:00 +0200

    Add backgroundimage

--- 02_02_frontend_empleados/src/App.js
+++ 02_02_frontend_empleados/src/App.js
@@ -1,61 +1,42 @@
-import './App.css';
-import { useContext, useEffect } from "react"
-import { Context } from './SharedState';
-import { get } from "./aux_api";
-import NavBar from './components/NavBar/NavBar';
-import Container from 'react-bootstrap/Container';
-import CarTable from './components/CarTable/CarTable';
-import { Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import ModificateCar from './components/ModificateCar/ModificateCar';
-import Uploads from './views/Uploads';
-import ClientTable from './components/ClientTable/ClientTable';
-import ModificateClient from './components/ModificateClient/ModificateClient';
-import Sale from './components/Sale/Sale';
-import SaleTable from './components/SaleTable/SaleTable';
-import ModificateSale from './components/ModificateSale/ModificateSale';
-import Rent from './components/Rent/Rent';
-import RentTable from './components/RentTable/RentTable';
+import "./App.css";
+import { useContext, useEffect } from "react";
+import { Context } from "./SharedState";
+import NavBar from "./components/NavBar/NavBar";
+import CarTable from "./components/CarTable/CarTable";
+import { Route, Routes } from "react-router-dom";
+import ModificateCar from "./components/ModificateCar/ModificateCar";
+import Uploads from "./views/Uploads";
+import ClientTable from "./components/ClientTable/ClientTable";
+import ModificateClient from "./components/ModificateClient/ModificateClient";
+import Sale from "./components/Sale/Sale";
+import SaleTable from "./components/SaleTable/SaleTable";
+import ModificateSale from "./components/ModificateSale/ModificateSale";
+import Rent from "./components/Rent/Rent";
+import RentTable from "./components/RentTable/RentTable";
 import Stack from "react-bootstrap/Stack";
-import ModificateRent from './components/ModificateRent/ModificateRent'
-import FormTable from './components/FormTable/FormTable'
-import SearchFor from './components/SearchFor/SearchFor';
-import Authorization from './components/Authorization/Authorization';
+import ModificateRent from "./components/ModificateRent/ModificateRent";
+import FormTable from "./components/FormTable/FormTable";
+import SearchFor from "./components/SearchFor/SearchFor";
+import Authorization from "./components/Authorization/Authorization";
 
 function App() {
+  const { actions } = useContext(Context);
 
-  const { states, actions } = useContext(Context);
-
-  useEffect(
-    () => {
-      actions.getAllCars()
-    },
-    []
-  )
-  useEffect(
-    () => {
-      actions.getAllClients()
-    },
-    []
-  )
-  useEffect(
-    () => {
-      actions.getAllSales()
-    },
-    []
-  )
-  useEffect(
-    () => {
-      actions.getAllRents()
-    },
-    []
-  )
-  useEffect(
-    () => {
-      actions.getAllForms()
-    },
-    []
-  )
+  useEffect(() => {
+    actions.getAllCars();
+  }, []);
+  useEffect(() => {
+    actions.getAllClients();
+  }, []);
+  useEffect(() => {
+    actions.getAllSales();
+  }, []);
+  useEffect(() => {
+    actions.getAllRents();
+  }, []);
+  useEffect(() => {
+    actions.getAllForms();
+  }, []);
 
   return (
     <>
@@ -80,7 +61,6 @@ function App() {
             <Route path="/searchFor" element={<SearchFor />} />
           </Routes>
         </Authorization>
-
       </Stack>
     </>
   );
