1e958ad3370caf9681cfe69e030f27b8c8fce627
portobuj <74266503+portoman@users.noreply.github.com>
2022-07-10 19:45:56 +0200

    Put €/dia to rent cars in carrousel

--- 02_01_frontend_clientes/src/components/CarCarrousel/CarCarrousel.jsx
+++ 02_01_frontend_clientes/src/components/CarCarrousel/CarCarrousel.jsx
@@ -1,54 +1,57 @@
-import { useEffect, useState, useRef } from 'react';
-import './CarCarrousel.css';
-import Carousel from 'react-bootstrap/Carousel';
-import { Link } from 'react-router-dom';
-import { useContext } from "react"
-import { Context } from "../../SharedState"
-import { PATH, host } from '../../defines';
+import { useEffect, useState, useRef } from "react";
+import "./CarCarrousel.css";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../../SharedState";
+import { PATH, host } from "../../defines";
 
 function CarCarrousel({ vehiculos }) {
+  const { states, actions } = useContext(Context);
 
-    const { states, actions } = useContext(Context);
+  let [coches, setCoches] = useState([]);
 
-    let [coches, setCoches] = useState([]);
+  useEffect(() => {
+    getCars();
+  }, [states]);
+  let contador = 0;
 
-    useEffect(
-        () => {
-            getCars()
-        },
-
-        [states]
-    )
-    let contador = 0
-    function getCars() {
-        setCoches(vehiculos.map(
-            (element) =>
-                <Carousel.Item key={contador++}>
-                    <Link to={"/vehiculo/" + element.id_coche}>
-                        <img
-                            className="d-block w-100"
-                            src={ host + PATH + element.foto}
-                            alt="First slide"
-                        />
-                    </Link>
-                    <Carousel.Caption>
-                        <h2>{element.marca}</h2>
-                        <h3>{element.modelo}</h3>
-                        <p>{element.precio}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-        ))
+  function devolverAlquilerOVenta(valor) {
+    let texto;
+    if (valor === 1) {
+      texto = "/ dia";
     }
+    return texto;
+  }
 
-
-    return (
-        <>
-            <Carousel className="carusel mx-auto">
-                {coches}
-            </Carousel>
-        </>
+  function getCars() {
+    setCoches(
+      vehiculos.map((element) => (
+        <Carousel.Item key={contador++}>
+          <Link to={"/vehiculo/" + element.id_coche}>
+            <img
+              className="d-block w-100"
+              src={host + PATH + element.foto}
+              alt="First slide"
+            />
+          </Link>
+          <Carousel.Caption>
+            <h2>{element.marca}</h2>
+            <h3>{element.modelo}</h3>
+            <p>
+              {element.precio} €{devolverAlquilerOVenta(element.alquiler)}
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))
     );
-}
+  }
 
+  return (
+    <>
+      <Carousel className="carusel mx-auto">{coches}</Carousel>
+    </>
+  );
+}
 
 export default CarCarrousel;
